818250934032853b71221ae5a56fdf7b4c8cfc16
samantha gard <samantha.gard13@gmail.com>
2024-09-30 15:21:13 -0400

    recipe page connection and added mongo db, need to work on switch data from local to atlas

--- src/pages/RecipePage.jsx
+++ src/pages/RecipePage.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import TopNavbar from "../components/TopNavbar";
+import { Container, Row, Col } from "react-bootstrap";
+import Footer from "../components/Footer";
 
 function RecipePage() {
   const { id } = useParams();
@@ -11,14 +13,16 @@ function RecipePage() {
   useEffect(() => {
     const fetchRecipe = async () => {
       try {
-        const response = await fetch(`http://localhost:3000/api/Handmade-Cravings/Appetizers/${id}`);
+        const response = await fetch(
+          `http://localhost:3000/api/Handmade-Cravings/Appetizers/${id}`
+        );
         if (!response.ok) {
-          throw new Error('Network response was not ok');
+          throw new Error("Network response was not ok");
         }
         const data = await response.json();
         setRecipe(data);
       } catch (error) {
-        console.error('Error fetching the recipe:', error);
+        console.error("Error fetching the recipe:", error);
         setError(error.message);
       } finally {
         setLoading(false);
@@ -39,28 +43,47 @@ function RecipePage() {
   return (
     <>
       <TopNavbar />
-      <div className="container mt-4">
-        {recipe ? (
+      <Container className="recipe-container" fluid>
+        {loading ? (
+          <p>Loading...</p>
+        ) : recipe ? (
           <div>
-            <h1>{recipe.name}</h1>
-            <img src={recipe.image_url} alt={recipe.name} />
-            <h3>Ingredients:</h3>
-            <div>
-              {recipe.ingredients.map((ingredient, index) => (
-                <div key={index}>{ingredient}</div>
-              ))}
-            </div>
-            <h3>Instructions:</h3>
-            <ol>
-              {recipe.instructions.map((instruction, index) => (
-                <li className="instruction-list" key={index}>{instruction}</li>
-              ))}
-            </ol>
+            <h1 className="recipe-title">{recipe.name}</h1>
+            <Row>
+              <Col lg={7} md={12} sm={12}>
+                <img
+                  src={recipe.image_url}
+                  alt={recipe.name}
+                  className="recipe-image"
+                />
+              </Col>
+              <Col lg={5} md={12} sm={12}>
+                <h3>Ingredients</h3>
+                <div>
+                  {recipe.ingredients.map((ingredient, index) => (
+                    <div key={index}>{ingredient}</div>
+                  ))}
+                </div>
+              </Col>
+            </Row>
+            <Row>
+              <Col lg={6} md={12} sm={12}>
+                <h3>Instructions</h3>
+                <ol>
+                  {recipe.instructions.map((instruction, index) => (
+                    <li className="instruction-list" key={index}>
+                      {instruction}
+                    </li>
+                  ))}
+                </ol>
+              </Col>
+            </Row>
           </div>
         ) : (
           <p>Recipe not found.</p>
         )}
-      </div>
+      </Container>
+      <Footer />
     </>
   );
 }
